667fdc6cf68757b0d406deffa9774bff153c4f50
Miguel Cruz <miguel.cruz@lifebeacon.com>
2024-06-06 18:26:06 -0600

    - add useEffect

--- src/hooks/useCallback.js
+++ src/hooks/useCallback.js
@@ -3,11 +3,32 @@ import { useEffect, useState } from "react"
 * deps es un array
 */
 export default function useCallback(callback, deps) {
-    const [state, setState] = useState(callback);
+    const [state, setState] = useState(callback());
 
     useEffect(() => {
-        setState(callback);
-    }, [deps])
+        const handeler = window.addListener.addEvent()
+
+        return () => {
+            window.removeEventListener(handeler);
+        }
+    }, [deps.name])
 
     return state;
+}
+
+
+
+export function useEffect2(callback, deps) {
+    const [_deps, setDeps] = useState(deps);
+
+    function comparaCambios(obj1, obj2) {
+    }
+    const hasChange = comparaCambios(_deps, deps);
+
+    if (hasChange) {
+        setDeps(deps);
+        callback();
+    }
+
+
 }
